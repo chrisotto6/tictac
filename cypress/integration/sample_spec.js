4a240d2627426bdef11c7d0b3c59d69945e60535
chrisotto6 <chrisotto6@gmail.com>
2019-01-16 12:48:05 -0600

    Fixing test spacing

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,7 +1,6 @@
 describe("Winning", function() {
   it("X Wins", function() {
     cy.visit("http://localhost:3000");
-
     cy.get("#0").click();
     cy.get("#move1").should("contain", "Go to move #1");
     cy.get("#3").click();
@@ -12,13 +11,11 @@ describe("Winning", function() {
     cy.get("#move4").should("contain", "Go to move #4");
     cy.get("#2").click();
     cy.get("#move5").should("contain", "Go to move #5");
-
     cy.contains("Winner: X");
   });
 
   it("O Wins", function() {
     cy.visit("http://localhost:3000");
-
     cy.get("#3").click();
     cy.get("#move1").should("contain", "Go to move #1");
     cy.get("#0").click();
@@ -31,7 +28,6 @@ describe("Winning", function() {
     cy.get("#move5").should("contain", "Go to move #5");
     cy.get("#2").click();
     cy.get("#move6").should("contain", "Go to move #6");
-
     cy.contains("Winner: O");
   });
 });
