1ebfd7a11657d5ee6678dd1e54c1a1a8291e6476
Chris Otto <chris.otto6@gmail.com>
2019-01-15 21:09:17 -0600

    Reintegrating the button checks to occur right after after a cell is selected

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,15 +3,14 @@ describe("Winning", function() {
     cy.visit("http://localhost:3000");
 
     cy.get("#0").click();
-    cy.get("#3").click();
-    cy.get("#1").click();
-    cy.get("#6").click();
-    cy.get("#2").click();
-
     cy.get("#move1").should("contain", "Go to move #1");
+    cy.get("#3").click();
     cy.get("#move2").should("contain", "Go to move #2");
+    cy.get("#1").click();
     cy.get("#move3").should("contain", "Go to move #3");
+    cy.get("#6").click();
     cy.get("#move4").should("contain", "Go to move #4");
+    cy.get("#2").click();
     cy.get("#move5").should("contain", "Go to move #5");
 
     cy.contains("Winner: X");
@@ -21,17 +20,16 @@ describe("Winning", function() {
     cy.visit("http://localhost:3000");
 
     cy.get("#3").click();
-    cy.get("#0").click();
-    cy.get("#6").click();
-    cy.get("#1").click();
-    cy.get("#7").click();
-    cy.get("#2").click();
-
     cy.get("#move1").should("contain", "Go to move #1");
+    cy.get("#0").click();
     cy.get("#move2").should("contain", "Go to move #2");
+    cy.get("#6").click();
     cy.get("#move3").should("contain", "Go to move #3");
+    cy.get("#1").click();
     cy.get("#move4").should("contain", "Go to move #4");
+    cy.get("#7").click();
     cy.get("#move5").should("contain", "Go to move #5");
+    cy.get("#2").click();
     cy.get("#move6").should("contain", "Go to move #6");
 
     cy.contains("Winner: O");
